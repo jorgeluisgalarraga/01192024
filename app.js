c74df9f119261585dc7bdf4fb1c8896ad268a7b6
Jorge Luis Galarraga <55800648+jorgeluisgalarraga@users.noreply.github.com>
2024-01-19 20:16:08 -0500

    Add new button to change color

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
+const body = document.querySelector("body") 
+const newButton = document.createElement("button")
+
+
 const changeColor = () => {
-    document.querySelector("body").style = "background-color: yellow;"
+    body.style = "background-color: yellow;"
+    if (body.style.backgroundColor == "yellow") {
+        body.append(newButton)
+        newButton.id = "newButton"
+        newButton.textContent = "Blue"
+    }
 }
 document.querySelector("button").addEventListener("click", changeColor)
+
+const blues = () => {
+    body.style.backgroundColor = "blue"
+}
+newButton.addEventListener("click", blues)
